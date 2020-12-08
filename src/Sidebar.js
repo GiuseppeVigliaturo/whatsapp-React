cfe7a78dea86febe6a15956b4825e9ef23da2819
giuviglia94 <vigliaturo94@gmail.com>
2020-12-08 15:59:03 +0100

    aggiunta sidebar-chat e chat utenti

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Sidebar.css';
+import SidebarChat from './SidebarChat';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import {IconButton, Avatar} from '@material-ui/core';
+import { SearchOutlined } from '@material-ui/icons';
 function Sidebar() {
     return (
         <div className="sidebar">
@@ -24,7 +26,16 @@ function Sidebar() {
             </div>
 
             <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined></SearchOutlined>
+                    <input type="text"/>
+                </div>
+            </div>
 
+            <div className="sidebar__chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
             </div>
         </div>
     )
