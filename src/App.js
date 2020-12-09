8042342bdada9c843c540ba5859bc3da0b159a06
giuviglia94 <vigliaturo94@gmail.com>
2020-12-09 18:39:42 +0100

    add pusher and connection to backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Pusher from 'pusher-js';
+import { useEffect } from 'react';
 function App() {
+
+  //useEffect permette di avviare del codice quando si avvia il componente
+  useEffect(() => {
+      const pusher = new Pusher('162ddb30fccd26320c0b', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app__body">
