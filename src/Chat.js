df307c63c0c52fe5ed612b0f0acfb24365859569
giuviglia94 <vigliaturo94@gmail.com>
2020-12-09 10:44:23 +0100

    add chat messages

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,58 @@
 import React from 'react';
 import './Chat.css';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { AttachFile, SearchOutlined } from '@material-ui/icons';
+import {IconButton, Avatar} from '@material-ui/core';
 function Chat() {
     return (
         <div className="chat">
-            <h1>Chat</h1>
+            <div className="chat__header">
+                <Avatar></Avatar>
+                <div className="chat__headerInfo">
+                    <h2>Nome utente</h2>
+                    <p>Ultimo messagio</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined></SearchOutlined>
+                    </IconButton>
+                        <IconButton>
+                        <AttachFile></AttachFile>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon></MoreVertIcon>
+                    </IconButton>  
+                </div>
+            </div>
+
+            <div className="chat__body">
+
+                <p className="chat__message">
+                    <span className="chat__name">
+                        Giuseppe
+                    </span>
+                    Messaggio
+                    <span className="chat__timestamp">
+                        {new Date().toDateString()}
+                    </span>
+                </p>
+
+                 <p className="chat__message chat__receiver">
+                    <span className="chat__name">
+                        Giuseppe
+                    </span>
+                    Messaggio
+                    <span className="chat__timestamp">
+                        {new Date().toDateString()}
+                    </span>
+                </p>
+
+
+
+
+
+
+            </div>
         </div>
     )
 }
